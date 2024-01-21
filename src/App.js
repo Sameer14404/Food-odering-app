dd9d13654ad0f39560aca31e5599d30d6859fb49
sameer dangi <sameerdangi66@gmail.com>
2024-01-21 08:08:09 -1200

    react-routing-done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 import Header from "./Header";
 import Body from "./Body";
-
-
-
-  
-
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import AboutUs from "./Aboutus";
+import Contact from "./Contact";
+import Error from "./Error";
 
 const Applayout=()=>{
    return(<div>
@@ -18,5 +16,28 @@ const Applayout=()=>{
    </div>)
 }
 
+const appRouter=createBrowserRouter([
+   {
+      path:"/",
+      element:<Applayout/>,
+      errorElement:<Error/>
+   },
+   {
+      path:"/about-us",
+      element:<AboutUs/>,
+     
+   },
+   {
+      path:"/contact",
+      element:<Contact/>,
+     
+   }
+
+])
+  
+
+
+
+
 let root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout/>);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/Error.js
@@ -0,0 +1,10 @@
+const Error=()=>{
+    return(
+        <div>
+            <h1>Ooops!!!!</h1>
+            <h2>Something Went wrong</h2>
+        </div>
+    )
+}
+
+export default Error; 
